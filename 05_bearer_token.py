d84331b1206fc429d08235b1ea2720744997b691
Luis Alberto Gonzalez M <100223263+Luis16121@users.noreply.github.com>
2024-06-22 12:03:49 -0600

    Recuperar_bearer_token

---
+++ 05_bearer_token.py
@@ -0,0 +1,18 @@
+from locust import HttpUser, task, constant
+
+class generar_token(HttpUser):
+    wait_time = constant(3)
+    host = "https://bookstore.demoqa.com"
+    token = ""
+
+    def on_start(self):
+        self.token()
+
+    @task
+    def token(self):
+        response = self.client.post("/account/v1/GenerateToken", json={"userName": "Qa2022","password": "QaPassword@2022"}, name="Generar_token")
+        if response.status_code == 200:
+            self.token = response.json().get("token")
+            print(f"Token exitoso: {self.token}")
+        else:
+            print(f"Token fallido: {response.status_code}")
